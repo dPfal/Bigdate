27229bba5fcb3129fa4cc7ad2ef834ec575080b7
dPfal <100793454+dPfal@users.noreply.github.com>
2023-04-16 19:39:57 +0900

    security 해제

--- src/main/java/gachon/bigdate/thenthen/ThenThenApplication.java
+++ src/main/java/gachon/bigdate/thenthen/ThenThenApplication.java
@@ -2,12 +2,13 @@ package gachon.bigdate.thenthen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ThenThenApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ThenThenApplication.class, args);
+		SpringApplication.run(ThenThenApplication.class, args
+		);
 	}
 
 }
