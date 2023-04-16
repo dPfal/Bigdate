acb7f03d90c61a0d54f843d2203e540c5b5f1ba3
dPfal <100793454+dPfal@users.noreply.github.com>
2023-04-17 01:28:00 +0900

    CORS 해결

--- src/main/java/gachon/bigdate/thenthen/controller/MainController.java
+++ src/main/java/gachon/bigdate/thenthen/controller/MainController.java
@@ -1,14 +1,19 @@
 package gachon.bigdate.thenthen.controller;
 
+import gachon.bigdate.thenthen.DTO.PlaceDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
-    @GetMapping("/")
-    public String index(){
-        return "hi";
+    @GetMapping("/places")
+    public PlaceDTO place(){
+        PlaceDTO place = new PlaceDTO();
+        place.setPlace_id(1234567890);
+        return place;
     }
 
 }
