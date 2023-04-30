b95c6518a5965c178916d9e9a5058571a0c50e79
dPfal <100793454+dPfal@users.noreply.github.com>
2023-04-30 23:13:02 +0900

    AuthController 주석 추가

--- src/main/java/gachon/bigdate/thenthen/controller/AuthController.java
+++ src/main/java/gachon/bigdate/thenthen/controller/AuthController.java
@@ -13,19 +13,19 @@ import java.util.Map;
 public class AuthController {
 
     private final AuthService authService;
-    @PostMapping("/join")
+    @PostMapping("/join") // 회원가입
     public ResponseEntity<String> join(@RequestBody UserDTO userDTO){
         return authService.join(userDTO.getUserId(),userDTO.getPassword()
                         ,userDTO.getUserName(),userDTO.getUserMood());
     }
 
-    @PostMapping("/login")
+    @PostMapping("/login") // 로그인
     public ResponseEntity<Map> login(@RequestBody UserDTO userDTO) throws Exception {
         //로그인시 JWT 발급
         return authService.login(userDTO.getUserId(),userDTO.getPassword());
     }
 
-    @GetMapping("/id")
+    @GetMapping("/id") //Id 중복체크
     public ResponseEntity<?> idDuplicateCheck(@RequestParam("userId") String userId ){
         return authService.idDuplicateCheck(userId);
     }
