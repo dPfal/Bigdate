148564836a8bbb2b0dc8b0a43b862402a9db13fa
dPfal <100793454+dPfal@users.noreply.github.com>
2023-05-08 19:51:24 +0900

    Config 수정

--- src/main/java/gachon/bigdate/thenthen/config/SecurityConfig.java
+++ src/main/java/gachon/bigdate/thenthen/config/SecurityConfig.java
@@ -53,6 +53,8 @@ public class SecurityConfig {
         config.addAllowedOrigin("http://localhost:60003");
         config.addAllowedOrigin("http://210.102.178.98:60003");
         config.addAllowedOrigin("http://210.102.178.98:3000");
+        config.addAllowedOrigin("http://ceprj.gachon.ac.kr:60003");
+        config.addAllowedOrigin("http://ceprj.gachon.ac.kr:3000");
         config.addAllowedOrigin("http://3.38.34.39:3000"); // 프론트 IPv4 주소
         config.addAllowedMethod("*"); // 모든 메소드 허용.
         config.addAllowedHeader("*");
