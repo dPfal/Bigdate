e960143ec403b16c58263b8d67c7764ef9729b56
dPfal <100793454+dPfal@users.noreply.github.com>
2023-05-22 23:46:57 +0900

    조회수 기능

--- src/main/frontend/bigdate/src/pages/Footer.js
+++ src/main/frontend/bigdate/src/pages/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+  
   return (
     <footer style={{ backgroundColor: '#f7f7f7', padding: '50px', textAlign: 'center', position: 'relative', transform: 'translateY(-100 %)', bottom: 0, width: '100%', fontFamily: 'Noto Sans KR', color:'#5F5F5F'}}>
       <img src='https://www.gachon.ac.kr/sites/kor/images/sub/slogan_3.png' style={{ width: '30px', margin: '10px' }}></img>
-      <span style={{fontWeight:'bold'}}>Gachon Univ - Team Bigdate</span><br/>
+      <span style={{ fontWeight: 'bold' }}>Gachon University </span><br/>
+      <span style={{ fontWeight: 'bold' }}>Team Bigdate</span><br />
         <span>References : </span>
         <a href='http://data.seoul.go.kr/dataList/OA-21285/A/1/datasetView.do' target='blank'>서울시 실시간 도시 데이터</a><br/>
         
