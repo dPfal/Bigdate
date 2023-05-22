9aaf67a157fd567e9f6bce92599ca0d2da4cf3ce
dPfal <100793454+dPfal@users.noreply.github.com>
2023-05-22 18:02:23 +0900

    마이페이지 수정

--- src/main/frontend/bigdate/src/pages/mypage/MyPostList.js
+++ src/main/frontend/bigdate/src/pages/mypage/MyPostList.js
@@ -79,7 +79,7 @@ function MyPostList() {
   
       return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div style={{display:'flex', width:'1200px',margin:'50px'}}>
+          <div style={{display:'flex',margin:'50px'}}>
             <div style={{ width: '250px', margin: '30px 30px' }}>
           <ListGroup>
             <ListGroup.Item action href='/mypage'active={false}>
